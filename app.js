ca45598d0533ee1c9bb396f5d7f2a57c8f86f524
MorningSol <98545619+MorningSol@users.noreply.github.com>
2022-04-11 15:30:22 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i])
-    }
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    console.log('==================');
-
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-     
-};
+    console.log('portfolio complete! Check out index.html to see the output!');
+});
 
 
 
-printProfileData(profileDataArgs);
+
+
+
+
+
+
+
+
